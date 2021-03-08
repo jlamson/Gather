c1c5d7a26c8e707337a3f2ea6494c8a2b1d43083
Joshua Lamson <joshua@truecoach.co>
2021-03-07 20:04:50 -0700

    [chore] refactor json file loading

--- scryfall/src/test/java/com/darkmoose117/scryfall/ScryfallObjectParsingTests.kt
+++ scryfall/src/test/java/com/darkmoose117/scryfall/ScryfallObjectParsingTests.kt
@@ -2,37 +2,18 @@ package com.darkmoose117.scryfall
 
 import com.darkmoose117.scryfall.data.Card
 import com.darkmoose117.scryfall.data.DataList
-import com.darkmoose117.scryfall.data.ScryfallObject
+import com.darkmoose117.scryfall.data.Ruling
+import com.darkmoose117.scryfall.data.Set
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
-import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Test
 import java.io.File
 
 class ScryfallObjectParsingTests {
-
-    companion object {
-
-        lateinit var jsonAsString: String
-
-        @JvmStatic
-        @BeforeClass
-        fun beforeClass() {
-            val classLoader = this.javaClass.classLoader
-            val file = classLoader.getResource("order_cmc_c_red_pow_3.json")?.let { url ->
-                File(url.file)
-            }
-            assertNotNull(file)
-            assert(file!!.exists())
-
-            jsonAsString = file.readText()
-        }
-    }
 
     lateinit var moshi: Moshi
 
@@ -43,8 +24,24 @@ class ScryfallObjectParsingTests {
             .build()
     }
 
+    private fun loadFileIntoString(fileName: String): String {
+        val classLoader = this.javaClass.classLoader
+        val file = classLoader.getResource(fileName)?.let { url ->
+            File(url.file)
+        }
+        assertNotNull(file)
+        assert(file!!.exists())
+
+        val jsonAsString = file.readText()
+        assert(jsonAsString.isNotBlank())
+
+        return jsonAsString
+    }
+
     @Test
     fun `should not crash on any card`() {
+        val jsonAsString = loadFileIntoString("order_cmc_c_red_pow_3.json")
+
         val responseType = Types.newParameterizedType(DataList::class.java, Card::class.java)
         val adapter: JsonAdapter<DataList<Card>> = moshi.adapter(responseType)
         val cardList = adapter.fromJson(jsonAsString)
