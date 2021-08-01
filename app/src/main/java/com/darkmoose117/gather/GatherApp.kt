c4395ba26f2cb071a426940d8546fa9144e8bac6
Joshua Lamson <joshua@truecoach.co>
2021-08-01 12:40:41 -0600

    [bugfix] Plant Timber, DUH

--- app/src/main/java/com/darkmoose117/gather/GatherApp.kt
+++ app/src/main/java/com/darkmoose117/gather/GatherApp.kt
@@ -6,8 +6,15 @@ import coil.ImageLoaderFactory
 import coil.util.CoilUtils
 import coil.util.DebugLogger
 import okhttp3.OkHttpClient
+import timber.log.Timber
 
 class GatherApp: Application(), ImageLoaderFactory {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
 
     override fun newImageLoader(): ImageLoader = ImageLoader.Builder(applicationContext)
         .crossfade(true)
